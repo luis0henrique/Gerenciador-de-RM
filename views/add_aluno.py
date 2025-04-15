31dec2ad35b0ac49b558b7352874409ee22ae16a
luis0henrique <luis0henrique@gmail.com>
2025-04-15 17:37:29 -0300

    .

--- views/add_aluno.py
+++ views/add_aluno.py
@@ -67,7 +67,7 @@ class AddAlunoWindow(QDialog, CenterWindowMixin):
         self._setup_buttons(content_layout)
 
     def _setup_table(self, layout):
-        """Configura a tabela de entrada do aluno"""
+        """Configurar a tabela de entrada do aluno"""
         self.table = QTableWidget(100, 2)
         self.table.setHorizontalHeaderLabels(["Nome do(a) Aluno(a)", "RM"])
         self.table.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
